0528c41e22bea9d5c488619dc94292fbb415ab2d
marve123 <eric.marvelboy@gmail.com>
2024-04-17 18:04:11 +0100

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      }
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
